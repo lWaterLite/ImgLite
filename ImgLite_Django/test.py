df491a11f42d022833284ac6beff7688e4029700
lWaterLite <lwaterlite@126.com>
2023-03-21 21:38:36 +0800

    bug fix

--- ImgLite_Django/test.py
+++
@@ -1,8 +0,0 @@
-from image.models import ImgIndex
-
-from django.core.paginator import Paginator
-
-imgIndexObjectList = ImgIndex.objects.filter(userIndex='1327654').order_by('imgUUID')
-imgIndexObjectListPage = imgIndexObjectList.count() // 12 + 1
-pageObject = Paginator(imgIndexObjectList, 15)
-print(pageObject.get_page(1).object_list)
